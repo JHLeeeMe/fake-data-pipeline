eb045ef52cd867c312dc2bb09d040ca145a04cc5
JHLeeeMe-T420 <lejung92@gmail.com>
2020-11-02 02:40:41 +0900

    [Modify] file name
    	이름 바꿈:     StreamHandler/src/resources/sql/create_databases.sql -> StreamHandler/src/resources/sql/create_database.sql
    	이름 바꿈:     StreamHandler/src/scripts/create_databases_script.sh -> StreamHandler/src/scripts/create_database_script.sh

---
+++ StreamHandler/src/scripts/create_database_script.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# for file path
+DIR="$(cd "$(dirname "$0")" && pwd -P)"
+
+psql -U postgres -d postgres -a -f $DIR/../resources/sql/create_database.sql 
--- StreamHandler/src/scripts/create_databases_script.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# for file path
-DIR="$(cd "$(dirname "$0")" && pwd -P)"
-
-psql -U postgres -d postgres -a -f $DIR/../resources/sql/create_databases.sql 
