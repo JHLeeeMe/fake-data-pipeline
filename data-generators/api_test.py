e90cd62a83c58551a7c8ee9d242c4006afc9f943
JHLeeeMe-msi <lejung92@gmail.com>
2020-12-04 22:48:36 +0900

    [Add] tmp pseudo python script
    	새 파일:       data-generators/api_test.py

---
+++ data-generators/api_test.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+
+import json
+import requests
+
+API_KEY = "<my-key>"
+
+res = requests.get('http://openapi.seoul.go.kr:8088/{}/json/bikeList'.format(API_KEY) + \
+                   '/1/1000/')
+
+if not res.ok:
+    sys.exit(res)
+
+res_json = json.loads(res.text)
+rent_bike_status = res_json['rentBikeStatus']
+result = rent_bike_status['RESULT']
+
+if result['CODE'] == 'INFO-000':
+    rows: list[dict] = rent_bike_status['row']
