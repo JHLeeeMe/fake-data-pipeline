0f1343b483d02a71c8c07cd488b20c439e32636c
JHLeeeMe-T420 <lejung92@gmail.com>
2020-12-06 12:45:00 +0900

    [Update] def get_source(), 변수 타입 지정 등.. 미완
    	수정함:        data-generators/api_test.py

--- data-generators/api_test.py
+++ data-generators/api_test.py
@@ -1,22 +1,46 @@
 #!/usr/bin/env python3
 
-
 import sys
 import json
-import requests
+from urllib import request
+from typing import List, Dict
 
 
-API_KEY = "<my-key>"
+with open('secrets/api_keys.json', 'r') as key_file:
+    API_KEY: str = json.load(key_file)['key']
 
-res = requests.get('http://openapi.seoul.go.kr:8088/{}/json/bikeList'.format(API_KEY) + \
-                   '/1/1000/')
+from_to = [1, 1000]
+while True:
+    if from_to[1] > 2000 :
+        from_to = [1, 1000]
 
-if not res.ok:
-    sys.exit(res)
+    data = get_source(API_KEY, from_to[0], from_to[1])
 
-res_json = json.loads(res.text)
-rent_bike_status = res_json['rentBikeStatus']
-result = rent_bike_status['RESULT']
+    data_json: Dict = json.loads(data)
+    bike_status: Dict = data_json['rentBikeStatus']
+    result: Dict = bike_status['RESULT']
 
-if result['CODE'] == 'INFO-000':
-    rows: list[dict] = rent_bike_status['row']
+    if result['CODE'] == 'INFO-000':
+        rows: List[Dict] = bike_status['row']
+
+    from_to[0] += 1
+    from_to[1] *= 2
+
+
+def get_source(API_KEY: str, _from: int = 1, _to: int = 1000) -> str:
+    """Get response
+
+    Keyword arguments:
+        API_KEY: str -- api key
+        _from: int = 1 -- start index
+        _to: int = 1000 -- end index
+    Return:
+        res.read().decode('utf8'): str
+    """
+    res = request.urlopen(
+            f'http://openapi.seoul.go.kr:8088/{API_KEY}/json/bikeList/{_from}/{_to}/')
+
+    if res.status != 200:
+        sys.exit(res)
+
+    return res.read().decode('utf8')
