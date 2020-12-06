164d5af535b31ed5425c50700d9e9eaa7d29cbfa
JHLeeeMe-T420 <lejung92@gmail.com>
2020-12-06 14:38:43 +0900

    [Update] Send msg to bike topic 미완
    	수정함:        api_test.py

--- data-generators/api_test.py
+++ data-generators/api_test.py
@@ -2,29 +2,11 @@
 
 import sys
 import json
+from time import sleep
 from urllib import request
 from typing import List, Dict
 
-
-with open('secrets/api_keys.json', 'r') as key_file:
-    API_KEY: str = json.load(key_file)['key']
-
-from_to = [1, 1000]
-while True:
-    if from_to[1] > 2000 :
-        from_to = [1, 1000]
-
-    data = get_source(API_KEY, from_to[0], from_to[1])
-
-    data_json: Dict = json.loads(data)
-    bike_status: Dict = data_json['rentBikeStatus']
-    result: Dict = bike_status['RESULT']
-
-    if result['CODE'] == 'INFO-000':
-        rows: List[Dict] = bike_status['row']
-
-    from_to[0] += 1
-    from_to[1] *= 2
+from kafka import KafkaProducer
 
 
 def get_source(API_KEY: str, _from: int = 1, _to: int = 1000) -> str:
@@ -44,3 +26,45 @@ def get_source(API_KEY: str, _from: int = 1, _to: int = 1000) -> str:
         sys.exit(res)
 
     return res.read().decode('utf8')
+
+
+BROKER = 'kafka-single-node:9092'
+TOPIC = 'bike'
+
+API_KEY_PATH = 'secrets/api_keys.json'
+
+with open(API_KEY_PATH, 'r') as key_file:
+    API_KEY: str = json.load(key_file)['key']
+
+try:
+    producer = KafkaProducer(
+            bootstrap_servers=BROKER,
+            value_serializer=lambda x: json.dumps(x).encode('utf-8')
+        )
+except Exception as e:
+    sys.exit(e)
+
+from_to = [1, 1000]
+msg = []
+while True:
+    if from_to[1] > 2000 :
+        producer.send(TOPIC, msg)
+        from_to = [1, 1000]
+        msg = []
+
+    data = get_source(API_KEY, from_to[0], from_to[1])
+
+    data_json: Dict = json.loads(data)
+    bike_status: Dict = data_json['rentBikeStatus']
+    result: Dict = bike_status['RESULT']
+
+    if result['CODE'] == 'INFO-000':
+        rows: List[Dict] = bike_status['row']
+    msg += rows
+
+    from_to[0] = from_to[1] + 1
+    from_to[1] += 1000
+
+    sleep(.5)
+
+
