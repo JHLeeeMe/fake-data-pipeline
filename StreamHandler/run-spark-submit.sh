3f17c34f5becb9e95539fd97a76a3cbcdab42a32
JHLeeeMe <lejung92@gmail.com>
2022-11-26 01:01:28 +0900

    [Modify] typo
    	수정함:        StreamHandler/run-spark-submit.sh
    	수정함:        StreamHandler/src/main/scala/StreamHandler.scala
    	수정함:        StreamHandler/src/scripts/create_database_script.sh
    	수정함:        StreamHandler/src/scripts/create_tables_script.sh
    	수정함:        data-generators/iot_devices.py
    	수정함:        data-generators/seoul_bike_api.py
    	수정함:        data-generators/words.py

--- StreamHandler/run-spark-submit.sh
+++ StreamHandler/run-spark-submit.sh
@@ -9,4 +9,4 @@ spark-submit \
   --packages "org.apache.spark:spark-sql-kafka-0-10_2.11:2.4.0" \
   --driver-class-path driver/postgresql-42.2.5.jar \
   --jars driver/postgresql-42.2.5.jar \
-  target/scala-2.11/stream-handler_2.11-1.0.jar 
+  target/scala-2.11/stream-handler_2.11-1.0.jar
--- StreamHandler/src/scripts/create_tables_script.sh
+++ StreamHandler/src/scripts/create_tables_script.sh
@@ -3,4 +3,4 @@
 # for file path
 DIR="$(cd "$(dirname "$0")" && pwd -P)"
 
-psql -U postgres -d pipeline_db -a -f $DIR/../resources/sql/create_tables.sql 
+psql -U postgres -d pipeline_db -a -f $DIR/../resources/sql/create_tables.sql
